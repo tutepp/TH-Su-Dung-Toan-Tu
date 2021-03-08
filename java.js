21c3f03221bb5a7632422f9559650cf587dbc724
tutepp <hoangtu1297hp@gmail.com>
2021-03-08 16:53:46 +0700

    TH Su Dung Toan Tu

---
+++ java.js
@@ -0,0 +1,24 @@
+<html>
+<head>
+<title>java.js</title>
+<meta http-equiv="Content-Type" content="text/html; charset=utf-8">
+<style type="text/css">
+.s0 { color: #0033b3;}
+.s1 { color: #080808;}
+.s2 { color: #067d17;}
+</style>
+</head>
+<body bgcolor="#ffffff">
+<table CELLSPACING=0 CELLPADDING=5 COLS=1 WIDTH="100%" BGCOLOR="#c0c0c0" >
+<tr><td><center>
+<font face="Arial, Helvetica" color="#000000">
+java.js</font>
+</center></td></tr></table>
+<pre><span class="s0">let </span><span class="s1">inputWidth = prompt(</span><span class="s2">&quot;Chiều dài&quot;</span><span class="s1">);</span>
+<span class="s0">let </span><span class="s1">inputHeight = prompt(</span><span class="s2">&quot;Rộng&quot;</span><span class="s1">);</span>
+<span class="s0">let </span><span class="s1">width = parseInt(inputWidth);</span>
+<span class="s0">let </span><span class="s1">height = parseInt(inputHeight);</span>
+<span class="s0">let </span><span class="s1">area = width * height;</span>
+<span class="s1">document.write(</span><span class="s2">&quot;Diện tích: &quot; </span><span class="s1">+ area);</span></pre>
+</body>
+</html>
